237b201f77de93b42e3a33d8456a1103568f0704
Saipranathi194 <saipranathiaraveti19@gmail.com>
2026-08-13 21:58:02 +0530

    Time: 1 ms (70.58%) | Memory: 44.5 MB (50.01%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(String oper :operations)
+        {
+            if(oper.contains("+"))
+            {
+                x++;
+            }
+            else
+            {
+                x--;
+            }
+        }
+        return x;
+    }
+}
